224a6d8e082ae69772138058c3285a69fbcdb229
Paul Armer <paul.armer@autodesk.com>
2021-07-03 14:38:25 -0700

    half-circle center

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 (() => {
-	const SEGMENTS_PER_ROW = [1, 8, 12, 16, 20]
+	const SEGMENTS_PER_ROW = [1, 9, 13, 17, 21]
 	const fan = document.querySelector('#fan')
 	const root = document.documentElement
 
@@ -26,27 +26,65 @@
 	const rows = SEGMENTS_PER_ROW.map((segmentsPerRow, rowIndex) => {
 		const segments = []
 		for (let s = 0; s < segmentsPerRow; s++) {
-			const start = {
-				r: rowIndex / SEGMENTS_PER_ROW.length + SPACING,
-				t: s / segmentsPerRow + (SPACING / 4),
-			}
-			const end = {
-				r: (rowIndex + 1) / SEGMENTS_PER_ROW.length,
-				t: (s + 1) / segmentsPerRow - (SPACING / 4),
-			}
+			const radiusPlus = (mod = 0) => (rowIndex + mod) * 0.85 / SEGMENTS_PER_ROW.length
+			const thetaPlus = (mod = 0) => (s + mod) / segmentsPerRow
 
-			console.log({ start, end })
-
-			segments.push({
-				BR: polarToTopLeft({ r: start.r, t: start.t }),
-				BL: polarToTopLeft({ r: start.r, t: end.t }),
-				TL: polarToTopLeft({ r: end.r, t: end.t }),
-				TR: polarToTopLeft({ r: end.r, t: start.t }),
-				tAvg: (end.t + start.t) / 2,
-				rAvg: (end.r + start.r) / 2,
-				rSmallPx: start.r * rMaxPx,
-				rLargePx: end.r * rMaxPx,
-			})
+			if (segmentsPerRow === 1) {
+				const anchor1 = polarToTopLeft({ r: radiusPlus(1), t: 0 })
+				const anchor2 = polarToTopLeft({ r: radiusPlus(1), t: 1 })
+				const radiusPx = radiusPlus(1.3) * rMaxPx
+				segments.push({
+					isCircle: true,
+					rAvg: radiusPlus(1),
+					tAvg: thetaPlus(0.5),
+					anchor1,
+					anchor2,
+					control1: {
+						left: anchor1.left,
+						top: anchor1.top - radiusPx,
+					},
+					control2: {
+						left: anchor2.left,
+						top: anchor2.top - radiusPx,
+					},
+				})
+			}
+			else {
+				segments.push({
+					rAvg: radiusPlus(1),
+					tAvg: thetaPlus(0.5),
+						rayRight: {
+							start: polarToTopLeft({
+								r: radiusPlus(-0.5),
+								t: thetaPlus(),
+							}),
+							end: polarToTopLeft({
+								r: radiusPlus(1),
+								t: thetaPlus(),
+							}),
+						},
+						rayMiddle: {
+							start: polarToTopLeft({
+								r: radiusPlus(-0.5),
+								t: thetaPlus(0.5),
+							}),
+							end: polarToTopLeft({
+								r: radiusPlus(1.5),
+								t: thetaPlus(0.5),
+							}),
+						},
+						rayLeft: {
+							start: polarToTopLeft({
+								r: radiusPlus(-0.5),
+								t: thetaPlus(1),
+							}),
+							end: polarToTopLeft({
+								r: radiusPlus(1),
+								t: thetaPlus(1),
+							}),
+						},
+					})
+			}
 		}
 		return segments
 	})
@@ -57,35 +95,57 @@
 	fan.onmouseup = () => { isDragging = false }
 	fan.onmouseleave = () => { isDragging = false }
 
-	rows.forEach(segments => {
+	rows.reverse().forEach(segments => {
 		segments.forEach(segment => {
 			const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-			const pathDescription = `M ${segment.BR.left} ${segment.BR.top
-				} A ${segment.rSmallPx} ${segment.rSmallPx} 0 0 0 ${segment.BL.left} ${segment.BL.top
-				} L ${segment.TL.left} ${segment.TL.top
-				} A ${segment.rLargePx} ${segment.rLargePx} 0 0 1 ${segment.TR.left} ${segment.TR.top
-				} Z`
+			if (segment.isCircle) {
+				path.setAttributeNS(
+					null,
+					'd',
+					`M ${segment.anchor1.left} ${segment.anchor1.top}` +
+						`C ${segment.control1.left} ${segment.control1.top} ` +
+							`${segment.control2.left} ${segment.control2.top} ` +
+							`${segment.anchor2.left} ${segment.anchor2.top} ` +
+						'Z'
+				)
+			}
+			else {
+				path.setAttributeNS(
+					null,
+					'd',
+					`M ${segment.rayLeft.start.left} ${segment.rayLeft.start.top} ` +
+						`L ${segment.rayMiddle.start.left} ${segment.rayMiddle.start.top} ` +
+						`L ${segment.rayRight.start.left} ${segment.rayRight.start.top} ` +
+						`C ${segment.rayRight.end.left} ${segment.rayRight.end.top} ` +
+							`${segment.rayRight.end.left} ${segment.rayRight.end.top} ` +
+							`${segment.rayMiddle.end.left} ${segment.rayMiddle.end.top} ` +
+						`C ${segment.rayLeft.end.left} ${segment.rayLeft.end.top} ` +
+							`${segment.rayLeft.end.left} ${segment.rayLeft.end.top} ` +
+							`${segment.rayLeft.start.left} ${segment.rayLeft.start.top} ` +
+						'Z'
+					)
+			}
 			const color = {
 				r: interpolateGenderChannel({ color: 'r', t: segment.tAvg, r: segment.rAvg }),
 				g: interpolateGenderChannel({ color: 'g', t: segment.tAvg, r: segment.rAvg }),
 				b: interpolateGenderChannel({ color: 'b', t: segment.tAvg, r: segment.rAvg }),
 			}
 			const rgb = `rgb(${color.r}, ${color.g}, ${color.b})`
+			path.setAttributeNS(null, 'fill', rgb)
+
 			const select = () => {
-				document.querySelectorAll('path').forEach(p => p.classList.remove(SELECTED))
+				document.querySelectorAll('path, circle').forEach(elem => elem.classList.remove(SELECTED))
 				path.classList.add(SELECTED)
 				root.style.setProperty('--gender', rgb)
-				root.style.setProperty('--text', `var(--${segment.rAvg  < 0.45 ? 'dark' : 'light'}-text)`)
+				root.style.setProperty('--text', `var(--${segment.rAvg  < 0.3 ? 'dark' : 'light'}-text)`)
 			}
-			path.setAttributeNS(null, 'd', pathDescription)
-			path.setAttributeNS(null, 'fill', rgb)
 			path.onmousedown = select
 			path.onmousemove = () => {
 				if (isDragging) {
 					select()
 				}
 			}
-			// path.setAttributeNS(null, 'stroke', 'black')
+
 			fan.appendChild(path)
 		})
 	})
